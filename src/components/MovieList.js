f2b1441fd5c16ba518d3413c121328acf311b894
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-10-06 20:53:30 +0300

    added MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,19 +4,31 @@ import MovieCard from './MovieCard'
 function MovieList() {
     const [movies, setMovies] = useState([])
 
-
-    useEffect(()=> {
-       fetch("https://moviefy-server-app.herokuapp.com/movies")
-       .then(res => res.json())
-       .then(data => setMovies(data))
+    useEffect(() => {
+      fetch("https://moviefy-server-app.herokuapp.com/movies")
+      .then(res => res.json())
+      .then(data => setMovies(data))
     }, [])
+
+    function handleClick(movieLikes, movieId) {
+      movieLikes = movieLikes + 1
+      fetch(`https://moviefy-server-app.herokuapp.com/movies/${movieId}`, {
+        method : "PATCH",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify(
+          {likes: movieLikes}
+        )
+      })
+    }
 
 
   return (
     <div className='movie-cards'>
-       {
-        movies.map(movie => <MovieCard movie={movie}/> )
-       }
+      {
+        movies.map(movie => <MovieCard movie={movie} handleClick={handleClick} />)
+      }
     </div>
   )
 }
