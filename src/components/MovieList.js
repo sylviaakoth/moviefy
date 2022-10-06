2e4e8954cf74da2abba41e6f595b0b9647f3c1e5
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-10-06 19:45:40 +0300

    add useEffect

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react'
 
 function MovieList() {
-    useState
-    useEffect
+    const [movies, setMovies] = useState([])
+
+
+    useEffect(()=> {
+       fetch("https://moviefy-server-app.herokuapp.com/movies")
+       .then(res => res.json())
+       .then(data => setMovies(data))
+    }, [])
   return (
     <div>MovieList</div>
   )
