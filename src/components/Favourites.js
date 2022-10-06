8057727d2dd75dc0871caa0ce60a7cabeb6f25ea
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-10-06 21:32:03 +0300

    added MovieCard

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import MovieCard from './MovieCard'
 
 function Favourites() {
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch("https://moviefy-server-app.herokuapp.com/movies")
+    .then(res => res.json())
+    .then(data => setMovies(data))
+  }, [])
+
+
+  const favouriteMovies = movies.filter(movie => movie.likes > 5)
+
+  function handleClick(movieLikes, movieId) {
+    movieLikes = movieLikes + 1
+    fetch(`https://moviefy-server-app.herokuapp.com/movies/${movieId}`, {
+      method : "PATCH",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(
+        {likes: movieLikes}
+      )
+    })
+  }
+
   return (
-    <div>Favourites</div>
+    <div>
+      {
+        favouriteMovies.map(movie => <MovieCard movie={movie} handleClick={handleClick}/>)
+      }
+    </div>
   )
 }
 
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function MovieCard({movie}) {
+function MovieCard({movie, handleClick}) {
   return (
     <div className="movie-card" key={movie.id}>
      <h1>{movie.title}</h1>
      <img src={movie.image} alt={movie.genre}/>
-     <p>{movie.likes} likes</p>
+     <p>{movie.likes} likes <button onClick={(e) => handleClick(movie.likes, movie.id)}>LIKE</button></p>
     </div>
 
   )
