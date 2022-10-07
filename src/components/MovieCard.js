6b5d621034252032134ac0beafbef97bf22289cf
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-10-07 17:59:33 +0300

    added movie-card to app.css

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,8 +4,8 @@ import "../App.css"
 function MovieCard({movie, handleClick}) {
   return (
     <div className="movie-card" key={movie.id}>
+    <img src={movie.image} alt={movie.genre}/>
      <h1>{movie.title}</h1>
-     <img src={movie.image} alt={movie.genre}/>
      <p>{movie.likes} likes <button onClick={(e) => handleClick(movie.likes, movie.id)}>LIKE</button></p>
     </div>
 
