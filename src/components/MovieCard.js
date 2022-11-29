e18f466f9f4617d6fa886c8c17c3e3d677075061
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-11-29 17:03:26 +0300

    Made changes to routing

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,11 +2,16 @@ import React from 'react'
 import "../App.css"
 
 function MovieCard({movie, handleClick}) {
+  // console.log(movie)
+
   return (
     <div className="movie-card" key={movie.id}>
     <img src={movie.image} alt={movie.genre}/>
      <h1>{movie.title}</h1>
-     <p>{movie.likes} likes <button onClick={(e) => handleClick(movie.likes, movie.id)}>LIKE</button></p>
+     {
+     movie.likes
+     }
+     <p> likes <button onClick={ ()=> handleClick(movie)}>LIKE</button></p>
     </div>
 
   )
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,37 +1,74 @@
-import React, { useEffect, useState } from 'react'
-import MovieCard from './MovieCard'
-import "../App.css"
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import "../App.css";
+import { useReducer } from 'react'
 
 function MovieList() {
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [refresh, newRefresh] = useState(0)
 
-    useEffect(() => {
-      fetch("https://moviefy-server-app.herokuapp.com/movies")
-      .then(res => res.json())
-      .then(data => setMovies(data))
-    }, [])
 
-    function handleClick(movieLikes, movieId) {
-      movieLikes = movieLikes + 1
-      fetch(`https://moviefy-server-app.herokuapp.com/movies/${movieId}`, {
-        method : "PATCH",
-        headers: {
-          "content-type": "application/json"
-        },
-        body: JSON.stringify(
-          {likes: movieLikes}
-        )
-      })
+  const initialState = {
+    likes: ""
+  }
+  const reducer = (state, action)=>{
+    switch(action.type){
+      case "INCREMENT":
+        return {...state, likes:  action.payload + 1};
+        case "NEWSTATE":
+          return {...state, likes:  action.payload};
+        
+        default:
+      return state;
     }
+  }
+  const [mylikes, dispatch] = useReducer(reducer, initialState)
+
+  function handleClick(movie) {
+    fetch(`https://moviefy-server-app.herokuapp.com/movies/${movie.id}`)
+    .then(res => res.json())
+    .then(data => {
+      dispatch({
+        type: "NEWSTATE",
+        payload: data.likes
+      })
+    })
+    dispatch({
+      type: "INCREMENT",
+      payload: movie.likes
+    })
+    console.log(movie);
+    console.log(mylikes)
+
+    fetch(`https://moviefy-server-app.herokuapp.com/movies/${movie.id}`, {
+      method : "PATCH",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(
+       {likes : mylikes.likes}
+      )
+    })
+    newRefresh(refresh+1)
+  }
+  useEffect(() => {
+    fetch("https://moviefy-server-app.herokuapp.com/movies")
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []);
 
 
   return (
-    <div className='movie-cards'>
-      {
-        movies.map(movie => <MovieCard movie={movie} handleClick={handleClick} key={movie.id} />)
-      }
+    <div className="movie-cards">
+      {movies ? (
+        movies.map((movie) => (
+          <MovieCard movie={movie} handleClick={handleClick} key={movie.id} />
+        ))
+      ) : (
+        <p>Loading!</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default MovieList
+export default MovieList;
